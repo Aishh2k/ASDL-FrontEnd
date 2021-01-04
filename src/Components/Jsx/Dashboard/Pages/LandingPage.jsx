30356fd89bd2697482a4a29029423a14cef47fd5
Aishh2k <aiswarya2000may@gmail.com>
2021-01-04 21:16:45 +0530

    Files

---
+++ src/Components/Jsx/Dashboard/Pages/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class LandingPage extends Component {
+    render() {
+        return (
+            <div id="main-content">
+                Landing Pageeeeeee
+            </div>
+        )
+    }
+}
+
+export default LandingPage
